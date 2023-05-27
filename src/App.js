9695d1a8602c6bf2c36de7a5e94240e42ac64fbd
Umar <omarsabiu679@gmail.com>
2023-05-27 12:20:35 +0100

    Handling Arrays in State

--- src/App.js
+++ src/App.js
@@ -1,105 +1,37 @@
 import { useState } from "react";
 
 const App = () => {
-  let [counter, setCounter] = useState(0);
-  const [clicks, setClicks] = useState({ left: 0, right: 0 });
+  const [right, setRight] = useState(0);
+  const [left, setLeft] = useState(0);
   const [allClicks, setAllClicks] = useState([]);
+  const [total, setTotal] = useState(0);
 
-  const handleLeftClick = () => {
-    const newClicks = {
-      ...clicks,
-      left: clicks.left + 1,
-    };
-    setClicks(newClicks);
+  const handleRight = () => {
+    setAllClicks(allClicks.concat("R"));
+    setTotal(total + 1);
+    return setRight(right + 1);
   };
-
-  const handleRightClick = () => {
-    setClicks(setAllClicks.concat("R"), { ...clicks, right: clicks.right + 1 });
+  const handleLeft = () => {
+    setAllClicks(allClicks.concat("L"));
+    setTotal(total + 1);
+    return setLeft(left + 1);
   };
-
-  const increasebyOne = () => {
-    setCounter(counter + 1);
-  };
-  const settoZero = () => {
-    setCounter(0);
-  };
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
-
-  return (
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-      <Hello name="Umar" age={26} />
-      {counter}
-      <button onClick={increasebyOne}> Plus</button>
-      <button onClick={settoZero}>Zero</button>
-
-      <p>
-        {clicks.left}
-        <button onClick={handleLeftClick}>Left</button>
-        <button onClick={handleRightClick}>Rigth</button>
-        {clicks.right}
-      </p>
-    </div>
-  );
-};
-const Hello = ({ name, age }) => {
-  const bornyear = () => {
-    const yearnow = new Date().getFullYear();
-    return yearnow - age;
+  const handleReset = () => {
+    return setLeft(0), setRight(0), setAllClicks([]), setTotal(0);
   };
 
   return (
     <>
-      <h1>
-        Hello {name} you are {age} years old
-      </h1>
-      <p>You are probably born in {bornyear()}</p>
+      <p>
+        {right}
+        <button onClick={handleRight}>Right</button>
+        <button onClick={handleLeft}>Left</button>
+        {left}
+        <button onClick={handleReset}>Reset</button>
+      </p>
+      {allClicks.join("-")}
+      <p>{total}</p>
     </>
-  );
-};
-const Header = ({ name }) => {
-  return <h1>{name}</h1>;
-};
-const Content = ({ parts }) => {
-  return (
-    <div>
-      <Part name={parts[0].name} exercises={parts[0].exercises} />
-      <Part name={parts[1].name} exercises={parts[1].exercises} />
-      <Part name={parts[2].name} exercises={parts[2].exercises} />
-    </div>
-  );
-};
-const Total = ({ parts }) => {
-  return (
-    <p>
-      Number of exercises{" "}
-      {parts[0].exercises + parts[1].exercises + parts[2].exercises}
-    </p>
-  );
-};
-const Part = ({ name, exercises }) => {
-  return (
-    <p>
-      {name} {exercises}
-    </p>
   );
 };
 
