5b53a2a5a1337d962a3fef11106165abf5065026
Umar <omarsabiu679@gmail.com>
2023-05-28 05:06:30 +0100

    Statistics Submission

--- src/App.js
+++ src/App.js
@@ -1,53 +1,84 @@
 import { useState } from "react";
-
 const App = () => {
-  const [right, setRight] = useState(0);
-  const [left, setLeft] = useState(0);
-  const [allClicks, setAllClicks] = useState([]);
-  const [total, setTotal] = useState(0);
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [average, setAverage] = useState(0);
 
-  const handleRight = () => {
-    setAllClicks(allClicks.concat("R"));
-    setTotal(total + 1);
-    return setRight(right + 1);
+  const goodHandler = () => {
+    setAll(all + 1);
+    setAverage(average + 1);
+    return setGood(good + 1);
   };
-  const handleLeft = () => {
-    setAllClicks(allClicks.concat("L"));
-    setTotal(total + 1);
-    return setLeft(left + 1);
+  const badHandler = () => {
+    setAll(all + 1);
+    setAverage(average - 1);
+    return setBad(bad + 1);
   };
-  const handleReset = () => {
-    return setLeft(0), setRight(0), setAllClicks([]), setTotal(0);
+  const neutralHandler = () => {
+    setAll(all + 1);
+    setAverage(average);
+    return setNeutral(neutral + 1);
   };
-
   return (
     <>
+      <h1>give feedback</h1>
       <p>
-        {right}
-        <Button handleclick={handleRight} text="Rigth" />
-        <Button handleclick={handleLeft} text="Left" />
-        {left}
-        <Button handleclick={handleReset} text="Reset" />
+        <Button text={"good"} buttonHandler={goodHandler} />
+        <Button text={"neutral"} buttonHandler={neutralHandler} />
+        <Button text={"bad"} buttonHandler={badHandler} />
       </p>
-      <p> Total: {total}</p>
-      <History allClicks={allClicks} />
+      <Statistics
+        good={good}
+        bad={bad}
+        average={average}
+        all={all}
+        neutral={neutral}
+      />
     </>
   );
 };
 
-const History = ({ allClicks }) => {
-  if (allClicks.length == 0) {
-    return (
-      <>
-        <p>The app works by clickinng the buttons</p>
-      </>
-    );
-  }
-
-  return <>Button Press History {allClicks.join(" ")}</>;
+const Button = ({ buttonHandler, text }) => {
+  return (
+    <>
+      <button onClick={buttonHandler}>{text}</button>
+    </>
+  );
 };
-const Button = ({ handleclick, text }) => {
-  return <button onClick={handleclick}>{text}</button>;
+const Statistics = ({ good, bad, average, all, neutral }) => {
+  if (all === 0) {
+    return <>No feedback given</>;
+  }
+  return (
+    <>
+      <h1>statistics</h1>
+      <table>
+        <tbody>
+          <StatisticsLine text="good" value={good} />
+          <StatisticsLine text="neutral" value={neutral} />
+          <StatisticsLine text="bad" value={bad} />
+          <StatisticsLine text="all" value={all} />
+          <StatisticsLine text="average" value={(average / all).toFixed(1)} />
+          <StatisticsLine
+            text="positive"
+            value={((good / all) * 100).toFixed(1).concat(" %")}
+          />
+        </tbody>
+      </table>
+    </>
+  );
+};
+const StatisticsLine = ({ text, value }) => {
+  return (
+    <>
+      <tr>
+        <td>{text} </td>
+        <td>{value}</td>
+      </tr>
+    </>
+  );
 };
 
 export default App;
